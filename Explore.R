0f87a16343e2b8ac080088af0ffb5e5ebce41934
pb8300a <36971609+pb8300a@users.noreply.github.com>
2019-03-12 17:23:27 -0400

    Update Explore.R

--- Explore.R
+++ Explore.R
@@ -7,25 +7,42 @@ serve_basic = read.csv("charting-m-stats-ServeBasics.csv",header = TRUE) #delete
 return_outcome = read.csv("charting-m-stats-ReturnOutcomes.csv",header = TRUE) #delete last 5 cols of excel spreadsheet
 
 
-serve_basic_1s = serve_basic[serve_basic$row == "1 1",]
-
-
+################################################################################
 #Who is in this dataset?
-Players = c(as.character(match_metadata$Player.1),as.character(match_metadata$Player.2))
-
+Players = c(toupper(as.character(match_metadata$Player.1)),toupper(as.character(match_metadata$Player.2)))
+proper=function(x){
+        paste0(toupper(substr(x, 1, 1)), tolower(substring(x, 2)))
+}
+Players = proper(Players)
 Total_Matches = sum(table(Players))
 Total_Players = length(unique(Players))
 Matches_Per_Player= sort(table(Players),decreasing = TRUE)
 sum(Matches_Per_Player[1:10])
 sum(Matches_Per_Player[Matches_Per_Player<20])
 
-top_100= barplot(Matches_Per_Player[1:100],width = rep(1,length(Matches_Per_Player)), col = "greenyellow",
-                xlab = "",ylab = "# Matches",xaxt = "n",ylim = c(0,325),
-                beside = TRUE,space = 0.1,lwd = 0.1)
 
-top_10= barplot(Matches_Per_Player[1:10],width = rep(1,10), col = "greenyellow",
-                xlab = "",ylab = "# Matches",xaxt = "n",ylim = c(0,325),
-                beside = TRUE,space = 0.1,lwd = 0.1)
-labs <- names(Matches_Per_Player)[1:10]
-text(cex=1, x=top_10-.25, y=-25, labs, xpd=TRUE, srt=45)
+####How many players, and how often are they repeated in this dataset?
+####This is not a random sample! The players are heavily skewed towards popular players.
+top_50= barplot(Matches_Per_Player[1:50],width = rep(1,50), col = "greenyellow",
+                xlab = "",ylab = "# Matches in Dataset",xaxt = "n",ylim = c(0,325),
+                beside = TRUE, 
+                main = paste("50 Players are involved in",
+                             sprintf("%1.0f%%",round(sum(Matches_Per_Player[1:50]/sum(Matches_Per_Player)*100))),
+                             "of the matches.",sep = " "))
+text(x=quantile(top_50,0.5), y=-20, "Individual Players", xpd=TRUE, cex = 1.2)
+
 box()
+top_10= barplot(Matches_Per_Player[1:10],width = rep(1,10), col = "greenyellow",
+                xlab = "",ylab = "# Matches in Dataset",xaxt = "n",ylim = c(0,325),
+                beside = TRUE,space = 0.1,lwd = 0.1, 
+                main = paste("10 Players are involved in",
+                             sprintf("%1.0f%%",round(sum(Matches_Per_Player[1:10]/sum(Matches_Per_Player)*100))),
+                             "of the matches.",sep = " "))
+labs <- names(Matches_Per_Player)[1:10]
+text(x=top_10-.25, y=-20, labs, xpd=TRUE, srt=45, cex = 0.8)
+box()
+
+library(wordcloud)
+wordcloud(words = names(Matches_Per_Player), freq = Matches_Per_Player, min.freq = 5,
+          max.words=200, random.order=FALSE, rot.per=0.35, 
+          colors=brewer.pal(8, "Dark2"))
