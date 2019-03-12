633496419d30a991d580a7ea0c57729c7d3e174e
pb8300a <36971609+pb8300a@users.noreply.github.com>
2019-03-12 16:42:34 -0400

    Update Explore.R

--- Explore.R
+++ Explore.R
@@ -4,6 +4,16 @@ match_metadata = read.csv("charting-m-matches.csv", header = TRUE) #delete ? in 
 raw_points = read.csv("charting-m-points.csv", header = TRUE)
 serve_direction = read.csv("charting-m-stats-ServeDirection.csv",header = TRUE) #delete last 5 cols of excel spreadsheet
 serve_basic = read.csv("charting-m-stats-ServeBasics.csv",header = TRUE) #delete last 5 cols of excel spreadsheet
+return_outcome = read.csv("charting-m-stats-ReturnOutcomes.csv",header = TRUE) #delete last 5 cols of excel spreadsheet
 
 
 serve_basic_1s = serve_basic[serve_basic$row == "1 1",]
+
+
+#Who is in this dataset?
+Players = c(as.character(match_metadata$Player.1),as.character(match_metadata$Player.2))
+
+Total_Matches = sum(table(Players))
+Total_Players = length(unique(Players))
+Matches_Per_Player= sort(table(Players),decreasing = TRUE)
+sum(Matches_Per_Player[1:10])
