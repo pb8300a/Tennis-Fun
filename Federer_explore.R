fe6d6b636f7ed543f4c5dab633c929ea0fa91766
pb8300a <36971609+pb8300a@users.noreply.github.com>
2019-03-13 13:54:37 -0400

    Update Federer_explore.R

--- Federer_explore.R
+++ Federer_explore.R
@@ -7,15 +7,18 @@ raw_points$num_id = seq(1:nrow(raw_points))
 match_lvl= match_metadata[match_metadata$Player.1 == "Roger Federer" | match_metadata$Player.2 == "Roger Federer", ]
 point_lvl = merge(raw_points,match_lvl , by = "match_id")
 point_lvl = point_lvl[order(point_lvl$num_id),]
-
+##############################
 #####For a given match
+##############################
 point_match1 = point_lvl[point_lvl$match_id == "19981005-M-Basel-R32-Andre_Agassi-Roger_Federer",]
 
+## A Timeline of the Match ##
+labs = c(as.character(point_match1$Player.1)[1],as.character(point_match1$Player.2)[1])
 par(mfrow =c(1,1),mar = c(5,5,2,1))
 plot(point_match1$PtWinner, type = "p", pch = 16,ylim = c(0,3), cex = 0.5,
      yaxt = "n",ylab = "",xlab = "",xaxt = "n")
 axis(side = 2, at = c(1,2), 
-     labels = c(as.character(point_match1$Player.1)[1],as.character(point_match1$Player.2)[1]),
+     labels = c(sub(" ", "\n", labs)),
      las = 2)
 
 last_pt = aggregate(point_match1$Pt, by = list(point_match1$Gm.), FUN = tail, n = 1)
