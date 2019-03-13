0597f25209692a0279513a5237b0afd490c5cf54
pb8300a <36971609+pb8300a@users.noreply.github.com>
2019-03-13 14:36:44 -0400

    Update Federer_explore.R

--- Federer_explore.R
+++ Federer_explore.R
@@ -1,3 +1,4 @@
+
 setwd("C:/Users/F400563/Desktop/tennis")
 data = read.csv("charting-w-stats-ServeBasics.csv")
 match_metadata = read.csv("charting-m-matches.csv", header = TRUE) #delete ? in the one column name
@@ -12,31 +13,40 @@ point_lvl = point_lvl[order(point_lvl$num_id),]
 ##############################
 point_match1 = point_lvl[point_lvl$match_id == "19981005-M-Basel-R32-Andre_Agassi-Roger_Federer",]
 
+
 ## A Timeline of the Match ##
-labs = c(as.character(point_match1$Player.1)[1],as.character(point_match1$Player.2)[1])
-par(mfrow =c(1,1),mar = c(5,5,2,1))
-plot(point_match1$PtWinner, type = "p", pch = 16,ylim = c(0,3), cex = 0.5,
-     yaxt = "n",ylab = "",xlab = "",xaxt = "n")
-axis(side = 2, at = c(1,2), 
-     labels = c(sub(" ", "\n", labs)),
-     las = 2)
+set_counter = table(rowSums(point_match1[,c("Set1","Set2")]))
+player_name = c(as.character(point_match1$Player.1)[1],as.character(point_match1$Player.2)[1])
+par(mfrow =c(length(set_counter),1),mar = c(1,5,1,1))
 
-last_pt = aggregate(point_match1$Pt, by = list(point_match1$Gm.), FUN = tail, n = 1)
-game_1 = aggregate(point_match1$Gm1.1, by = list(point_match1$Gm.), FUN = tail, n = 1)
-game_2 = aggregate(point_match1$Gm2.1, by = list(point_match1$Gm.), FUN = tail, n = 1)
-game_2 = game_2[order(as.numeric(as.character(game_2$Group.1))),]
-
-all = merge(last_pt, game_1, by = "Group.1")
-all = merge(all, game_2, by = "Group.1")
-all =all[order(as.numeric(as.character(all$Group.1))),]
-names(all) = c("Group", "Pt", "Gm1","Gm2")
-count = 0
-lastpoint=0
-for(i in all$Pt){
-        if(count != 0){lastpoint = all$Pt[count]}
-        midpoint = (lastpoint + i+1)/2
-        count =count+1
-        abline(v = i+0.5, col = "gray")
-        text(x=midpoint,y = 0.5, all$Gm1[count])
-        text(x=midpoint,y = 2.5, all$Gm2[count])
+for(i in names(set_counter)){
+        i = "1"
+        point_set=point_match1[rowSums(point_match1[,c("Set1","Set2")])==as.numeric(i),]
+        
+        plot(point_set$PtWinner, type = "p", pch = 16,ylim = c(0,3), cex = 0.5,
+             yaxt = "n",ylab = "",xlab = "",xaxt = "n")
+        axis(side = 2, at = c(1,2), labels = c(sub(" ", "\n", player_name)),las = 2)
+        
+        last_pt = aggregate(point_set$Pt, by = list(point_set$Gm.), FUN = tail, n = 1)
+        game_1 = aggregate(point_set$Gm1.1, by = list(point_set$Gm.), FUN = tail, n = 1)
+        game_2 = aggregate(point_set$Gm2.1, by = list(point_set$Gm.), FUN = tail, n = 1)
+        
+        all = merge(last_pt, game_1, by = "Group.1")
+        all = merge(all, game_2, by = "Group.1")
+        names(all) = c("Group", "Pt", "Gm1","Gm2")
+        
+        if(i !="0"){all$Pt = all$Pt - all$Pt[1] +(all$Pt[1] - point_set$Pt[1] +1)}
+        all =all[order(as.numeric(as.character(all$Group))),]
+        count = 0
+        lastpoint=0
+        for(j in all$Pt){
+                if(count != 0){lastpoint = all$Pt[count]}
+                midpoint = (lastpoint + j+1)/2
+                count =count+1
+                abline(v = j+0.5, col = "gray")
+                text(x=midpoint,y = 0.5, all$Gm1[count])
+                text(x=midpoint,y = 2.5, all$Gm2[count])
+        }
+      
 }
+
