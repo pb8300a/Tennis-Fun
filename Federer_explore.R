0c6894204c50d8ef10b8474b8d002fa1ff2f563c
pb8300a <36971609+pb8300a@users.noreply.github.com>
2019-03-13 13:49:10 -0400

    Update Federer_explore.R

--- Federer_explore.R
+++ Federer_explore.R
@@ -10,10 +10,30 @@ point_lvl = point_lvl[order(point_lvl$num_id),]
 
 #####For a given match
 point_match1 = point_lvl[point_lvl$match_id == "19981005-M-Basel-R32-Andre_Agassi-Roger_Federer",]
-service_winners = cumsum(point_match1$isSvrWinner[point_match1$Svr == 1])
 
-par(mfrow =c(1,1))
-plot(point_match1$PtWinner, type = "p", pch = 16,ylim = c(0,3), cex = 0.5)
+par(mfrow =c(1,1),mar = c(5,5,2,1))
+plot(point_match1$PtWinner, type = "p", pch = 16,ylim = c(0,3), cex = 0.5,
+     yaxt = "n",ylab = "",xlab = "",xaxt = "n")
+axis(side = 2, at = c(1,2), 
+     labels = c(as.character(point_match1$Player.1)[1],as.character(point_match1$Player.2)[1]),
+     las = 2)
 
 last_pt = aggregate(point_match1$Pt, by = list(point_match1$Gm.), FUN = tail, n = 1)
-for(i in last_pt$x){abline(v = i, col = "gray")}
+game_1 = aggregate(point_match1$Gm1.1, by = list(point_match1$Gm.), FUN = tail, n = 1)
+game_2 = aggregate(point_match1$Gm2.1, by = list(point_match1$Gm.), FUN = tail, n = 1)
+game_2 = game_2[order(as.numeric(as.character(game_2$Group.1))),]
+
+all = merge(last_pt, game_1, by = "Group.1")
+all = merge(all, game_2, by = "Group.1")
+all =all[order(as.numeric(as.character(all$Group.1))),]
+names(all) = c("Group", "Pt", "Gm1","Gm2")
+count = 0
+lastpoint=0
+for(i in all$Pt){
+        if(count != 0){lastpoint = all$Pt[count]}
+        midpoint = (lastpoint + i+1)/2
+        count =count+1
+        abline(v = i+0.5, col = "gray")
+        text(x=midpoint,y = 0.5, all$Gm1[count])
+        text(x=midpoint,y = 2.5, all$Gm2[count])
+}
